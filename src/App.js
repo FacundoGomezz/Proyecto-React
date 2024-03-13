22cd6254651fac3630b1ff2374d572c9c6f76d69
facugomez <facugomez0902@gmail.com>
2024-03-13 16:06:57 -0300

    Proyecto React

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import ItemListContainer from './components/ItemListContainer';
 import NavBar from './components/Navbar';
+import ItemListContainer from './components/ItemListContainer';
+
 
 function App() {
   const brandName = "TutuMarket";
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,12 +1,10 @@
-// En el archivo ItemListContainer.js
-
 import React from 'react';
 
 const ItemListContainer = ({ greeting }) => {
   return (
     <div className="item-list-container" style={{ backgroundColor: '#f0f0f0', padding: '20px', borderRadius: '5px', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)' }}>
       <h2 style={{ color: '#333' }}>{greeting}</h2>
-      {/* Aquí puedes agregar más contenido si es necesario */}
+      {}
     </div>
   );
 };
